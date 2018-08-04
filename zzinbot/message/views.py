e90bb59f3768e9ffebd4836530520b6276eee137
tinyjin <baram991103@gmail.com>
2018-08-04 12:33:10 +0900

    off csrf secure

--- zzinbot/message/views.py
+++ zzinbot/message/views.py
@@ -2,15 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 import json
 
-
+@csrf_exempt
 def index(request):
-  message = {
-    "message":{
-        "text" : "안녕~? 나는 찐봇이야!"
+  if request.method == "POST" :  
+    message = {
+      "message":{
+          "text" : "안녕~? 나는 찐봇이야!"
+      }
     }
-  }
 
-  jsonObj = json.dumps(message, ensure_ascii=False)
-  return HttpResponse(jsonObj, content_type = 'application/json; charset=utf-8')
+
+    jsonObj = json.dumps(message, ensure_ascii=False)
+    return HttpResponse(jsonObj, content_type = 'application/json; charset=utf-8')
