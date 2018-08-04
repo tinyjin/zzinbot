708f504486d9c3165da33ab1475e3a842a7a413a
tinyjin <baram991103@gmail.com>
2018-08-04 21:43:32 +0900

    make response a random

--- zzinbot/message/views.py
+++ zzinbot/message/views.py
@@ -4,16 +4,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
+import random
 
 @csrf_exempt
 def index(request):
+  resChat = [
+    '안녕~? 나는 찐봇이야!',
+    '반가워~ 친구야~',
+    '나랑.. 친구할래?',
+    '나랑 놀자 ^^'
+  ]
+
   if request.method == "POST" :  
     message = {
       "message":{
-          "text" : "안녕~? 나는 찐봇이야!"
+          "text" : random.choice(resChat)
       }
     }
-
 
     jsonObj = json.dumps(message, ensure_ascii=False)
     return HttpResponse(jsonObj, content_type = 'application/json; charset=utf-8')
